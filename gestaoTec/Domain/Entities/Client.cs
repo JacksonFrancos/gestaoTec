f995b315b715af75def1d8daba348653815c9be8
@jsantos01 <jsantos01@reconcavo.org.br>
2025-06-27 15:24:35 -0300

    Add project files.

---
+++ gestaoTec/Domain/Entities/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestaoTec.Domain.Entities
+{
+    internal class Client
+    {
+        public  Guid ClientId { get; set; }
+        public string Name { get; set; }
+
+        public int Number { get; set; }
+        public string  Email { get; set; }
+
+        public string Address { get; set; }
+    }
+}
