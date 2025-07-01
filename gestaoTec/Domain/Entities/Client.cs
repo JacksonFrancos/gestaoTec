c9777ce20ce9e4e71aec1ff8cb74035eadd81b65
@jsantos01 <jsantos01@reconcavo.org.br>
2025-07-01 14:39:55 -0300

    Criando as entidades

--- gestaoTec/Domain/Entities/Client.cs
+++ gestaoTec/Domain/Entities/Client.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,39 @@ namespace gestaoTec.Domain.Entities
         public string  Email { get; set; }
 
         public string Address { get; set; }
+
+        // conceito de Invariantes 
+        // esse metodo é chamado de Factory Method 
+        public static Client Create ( String name , string email, string address , Guid clientID)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("O nome não pode ser nullo", nameof(name));
+            }
+            return new Client
+            {
+                ClientId = clientID,
+                Name = name,
+                Email = email,
+                Address = address,
+                Number = 0
+            };
+
+        }
+        public void Update(string name, string email, string address)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("O nome não pode ser nullo", nameof(name));
+            }
+            Name = name;
+            Email = email;
+            Address = address;
+        }
+
     }
+
+
+    
+
 }
---
+++ gestaoTec/Domain/Entities/Equipment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestaoTec.Domain.Entities
+{
+    internal class Equipment
+    {
+        public string EquipName { get; set; }
+        public string EquipType { get; set; }
+
+        public string EquipmentModel  { get; set; }
+        public string EquipmentMark { get; set; }
+    }
+}
