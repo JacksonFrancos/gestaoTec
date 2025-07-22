d46d5b8019b3e9b14fde6b38195f1b25625bf56b
@jsantos01 <jsantos01@reconcavo.org.br>
2025-07-22 08:58:15 -0300

    mudanças no init do projeto

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -3,29 +3,28 @@ using gestaoTec.Application.Services.Iservices;
 using gestaoTec.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<GestTecContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddControllers();
-builder.Services.AddScoped<IClientService, ClientService>();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// <- MUITO IMPORTANTE
+app.MapFallbackToFile("index.html");
 
 app.Run();
