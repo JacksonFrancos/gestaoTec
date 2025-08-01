0b74f13efd7820f145fefcdecbebc84b93a8835e
@jsantos01 <jsantos01@reconcavo.org.br>
2025-08-01 14:52:49 -0300

    direcionando o webapi para abrir as interfaces do front

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -9,7 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "wwwroot";
+});
 var app = builder.Build();
 
 app.UseSwagger();
@@ -17,14 +20,20 @@ app.UseSwaggerUI();
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
-
+app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseSpaStaticFiles();
+app.UseRouting();
 // <- MUITO IMPORTANTE
 app.MapFallbackToFile("index.html");
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "wwwroot";
+});
 
 app.Run();
